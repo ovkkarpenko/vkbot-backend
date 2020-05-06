f36986bfffd6b571eb81899273057b174a91cd21
Administrator <admin@example.com>
2020-05-06 10:14:24 +0200

    init

---
+++ src/test/java/com/sanyakarpenko/vkbot/VkbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanyakarpenko.vkbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
