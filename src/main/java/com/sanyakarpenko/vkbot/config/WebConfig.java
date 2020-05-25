35767a94e64362437a8e709de3f8185e703147a8
Administrator <admin@example.com>
2020-05-25 18:08:58 +0200

    up

--- src/main/java/com/sanyakarpenko/vkbot/config/WebConfig.java
+++ src/main/java/com/sanyakarpenko/vkbot/config/WebConfig.java
@@ -12,4 +12,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedOrigins("https://vkbot-noves.herokuapp.com");
     }
+    //
 }
