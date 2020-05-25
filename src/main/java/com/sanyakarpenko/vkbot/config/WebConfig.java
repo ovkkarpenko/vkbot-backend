62dd48f428c6f30898406861bf6b06373dc3f4fe
Administrator <admin@example.com>
2020-05-25 17:15:44 +0200

    up

--- src/main/java/com/sanyakarpenko/vkbot/config/WebConfig.java
+++ src/main/java/com/sanyakarpenko/vkbot/config/WebConfig.java
@@ -10,6 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("https://vkbot-noves.herokuapp.com");
     }
 }
